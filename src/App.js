f60164c1e5393530ac4c242103790a7660248277
aatifnehal786 <aatifnehal786@gmail.com>
2025-12-22 20:36:09 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/user-login/Login';
+
+function App() {
+  return (
+    <div className="bg-black text-white">
+      Hello aatif
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/user-login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Login = ()=> {
+
+
+    return (
+        <div className="bg-black text-white text-lg">
+            Login
+        </div>
+    )
+}
+
+
+export default Login;
