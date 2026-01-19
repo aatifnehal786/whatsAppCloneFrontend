7465a02627860b73aa4ae03b737c943f7c4f4043
aatifnehal786 <aatifnehal786@gmail.com>
2026-01-19 23:13:24 +0530

    .

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -9,13 +9,13 @@ const axiosInstance = axios.create({
   withCredentials:true
 });
 
-// Attach token to every request
-// axiosInstance.interceptors.request.use((config) => {
-//   const token = getToken();
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default axiosInstance;
