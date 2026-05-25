6e19df843a41404aaed591add994949ee2bf4df5
aatifnehal786 <aatifnehal786@gmail.com>
2026-05-25 17:10:41 +0530

    .

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
