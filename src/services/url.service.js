5f4063ef2f6f9b9cd367e4aa4883bf770d723cc1
aatifnehal786 <aatifnehal786@gmail.com>
2026-05-25 17:04:53 +0530

    .

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "https://whatsappclonebackend-149w.onrender.com/api";
-
-
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
